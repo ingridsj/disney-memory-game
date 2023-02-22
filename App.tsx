d61816628ff4269cce5066650a34518b78e1b6ad
ingridsj <ingridsjalmeida@gmail.com>
2023-02-22 19:53:48 -0300

    adicionando confete ao Result Modal #14

--- App.tsx
+++ App.tsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react'
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import ConfettiCannon from 'react-native-confetti-cannon';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, PrincessSofia_400Regular} from '@expo-google-fonts/princess-sofia';
 
 import { theme } from './src/utils/theme'
 
-import MemoryCard from './src/components/MemoryCard';
 import Button from './src/components/Button';
-import { View } from 'react-native';
-import Modal from './src/components/Modal';
 import Label from './src/components/Label';
+import ModalMenu from './src/components/ModalMenu';
+import ModalResult from './src/components/ModalResult';
 
 export default function App() {
-  const [ open, setOpen ] = useState(false)
+  const [ openMenu, setOpenMenu ] = useState(false)
+  const [ openResult, setOpenResult ] = useState(false)
   
   let [fontsLoaded] = useFonts({
 		PrincessSofia_400Regular,
@@ -24,29 +25,25 @@ export default function App() {
 
   return (
   <ThemeProvider theme={theme}>
-    <GestureHandlerRootView style={{
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-      }}>
-        <MemoryCard princessName='bela' selected={true} visible={true} />
-        <Button backgroundColor='purple' onPress={() => setOpen(true)}>
-          <Label 
-            text='Novo' 
-            color={theme.colors.pink} 
-            fontFamily={theme.fonts.princessSofia}
-          />
+    <GestureHandlerRootView style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+        {openResult && <ConfettiCannon count={50} origin={{x: -10, y: 0}} />}
+        <Label text='Memória'  color={theme.colors.pink}  fontSize={48} />
+        <Button backgroundColor='pink' onPress={() => setOpenResult(true)}>
+          <Label text='Reiniciar' color={theme.colors.purple} fontSize={18} />
         </Button>
-        <Modal open={open} onClosed={() => setOpen(false)}> 
-          <View>
-            <Label 
-              text='Tamanhos:' 
-              color={theme.colors.purple} 
-              fontFamily={theme.fonts.princessSofia}
-            />
-          </View>
-        </Modal>
-      </GestureHandlerRootView>
+        <Button backgroundColor='purple' onPress={() => setOpenMenu(true)}>
+          <Label text='Novo' color={theme.colors.pink} fontSize={18} />
+        </Button>
+        <ModalMenu
+          open={openMenu}
+          setOpen={setOpenMenu}
+        />
+        <ModalResult
+          open={openResult}
+          setOpen={setOpenResult}
+          setOpenMenu={setOpenMenu}
+        /> 
+    </GestureHandlerRootView>
   </ThemeProvider>
   );
 }
